c4df5fd5b3df721f1a4b7ff400eaef9ae9b942d4
Prathmesh895 <125139465+Prathmesh895@users.noreply.github.com>
2024-05-24 11:34:21 +0530

    24/05/2025

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -6,15 +6,15 @@ import Dashboard from '@/components/adminDashboard/dashboard';
 export default  async function Page() {
   const session = await getServerSession(authOptions);
    
-  // if (!session) {
-  //   redirect('/Login');
-  //   return null; // Ensure that the function returns null after redirection
-  // }
+  if (!session) {
+    redirect('/Login');
+    return null; // Ensure that the function returns null after redirection
+  }
 
-  // if (session && session.user.role !== 'admin') {
-  //   redirect('/denied');
-  //   return null; // Ensure that the function returns null after redirection
-  // }
+  if (session && session.user.role !== 'admin') {
+    redirect('/denied');
+    return null; // Ensure that the function returns null after redirection
+  }
   return (
     <>
     <Dashboard/>
---
+++ src/app/mycourses/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import BG from '/public/course-breadcramb.png'
+export default function page() {
+  return (
+    <>
+      <section className=''>
+        <div className='absolute -z-10 '>
+          <Image src={BG} alt="coursebg" className='relative h-52' />
+        </div>
+        <div className='lg:mx-44 mx-5 py-12'>
+          <div className='flex text-white space-x-3 my-2'>
+            <h1>Home  &gt; </h1>
+            <h1>My courses</h1>
+          </div>
+          <h1 className='lg:text-5xl text-4xl text-white font-semibold'>My courses</h1>
+        </div>
+      </section>
+    </>
+  )
+}
